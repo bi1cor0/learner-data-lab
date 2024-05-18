0a05d6d9b1629d417cea2ff64aaf09b545297ccb
yeahbudd01 <106033471+yeahbudd01@users.noreply.github.com>
2024-05-18 11:47:34 -0700

    Added a try and catch if statement in the main getLearner data function.

--- maincode-sba308.mjs
+++ maincode-sba308.mjs
@@ -83,16 +83,17 @@ const CourseInfo = {
       let learner_id = s.learner_id; //found out you can set the iterator var in a for of loop can be used to call keys themselves within an array. 
       let assignmentid = s.assignment_id; //i'm using the value of the object in the array ids and putting it in a holder variable to be used in this loop.
 
-      const isIDsame = resultArr.find(item => item.id === learner_id); //i found and read up on the .find function. the function's output is actually the instance where 
-      if(!isIDsame) {
+      const isIDsame = resultArr.find(item => item.id === learner_id); //i found and read up on the .find function. the find function searches for the first instance of what I set the function to find.
+      // In this case: if the iterator: item.id in the resultArr array, has the same learner_id as the LearnerSubmissions, .find will send out the object, which I put in a var.                   
+      if(!isIDsame) { //now if isIDSsame is false (empty) then a new object within the array will start. 
         resultArr.push({[assignmentid]: s.submission,
-          id: learner_id
-        }) 
-      } else { 
-          isIDsame[assignmentid] = s.submission;
-        }
-      
-    }
+          id: learner_id //the resultArr will push out an object with the assignmentid key name with the value of whatever iterator s found in .submission. along with the id of the learner. this will in turn create a new object in the array. 
+        } )  
+      } else {  //if isIDsame is true (has a value) then it will add on to the object inside.
+          isIDsame[assignmentid] = s.submission; //the assignmentid will uptick, and will create a new entry in the object of isIDsame. isIDsame is actually referencing an object directly onto the resultArr array.
+        }//so, whatever I do to the object isIDsame, I do to the object in resultArr. In this case, I'm reassigning the assignment ID in the object entry, and includeing the submission
+        //once the loop continues, resultArr should have more entries in the object value in the array for as long as item.id and learner_id are the same. 
+    } 
     return resultArr;
   }
 
@@ -105,6 +106,10 @@ function isDue(dueDate, submissionDate) { //if statement used to check due date.
   return Date.parse(submissionDate) < Date.parse(dueDate);
 }
 
+//part 4 put it all together
+//create a function that takes the reformatted LearnerSubmitions and extract the necessary data. First take the student ID, and then iterate through the LearnerSubmissions. 
+//Second, take the learner assignments and manipulate the scores. Create a variable to take the values of each object to reference later in the weighted avg scores. 
+
 function allTogether(studentObj, assignArr){
   let count = 0;
   let studentSum = 0;
@@ -113,10 +118,6 @@ function allTogether(studentObj, assignArr){
   let results = {
     id: studentID
   }
-
-  //part 4 put it all together
-//create a function that takes the reformatted LearnerSubmitions and extract the necessary data. First take the student ID, and then iterate through the LearnerSubmissions. 
-//Second, take the learner assignments and manipulate the scores. Create a variable to take the values of each object to reference later in the weighted avg scores. 
 
   for(let c in studentObj){
     if(c == 'id') {continue;}
@@ -133,20 +134,29 @@ function allTogether(studentObj, assignArr){
 
 //////main function/////////////////////////
 function getLearnerData(courseinfo, assignmentgroups, learnersubs) {
-    //part 1, create an array of objects that takes the assignnments from the assignment groups.
-    let mainMents = assignmentgroups.assignments;
-    let transformed = transformLearner(learnersubs); //created new variable that takes the new reformatted learnersubs array.
 
-    let mainResults = [];
+  //part 1, create an array of objects that takes the assignnments from the assignment groups.  
+  let mainMents = assignmentgroups.assignments;
+  let transformed = transformLearner(learnersubs); //created new variable that takes the new reformatted learnersubs array.
+  let mainResults = [];
+  
+  //part 5 implement try catch statement with if else
+    try {
+      if (courseinfo.id === assignmentgroups.course_id){
+        for(let i = 0; i < transformed.length; i++){
+          mainResults.push(allTogether(transformed[i], mainMents))
+        }
+        return mainResults;
+        } else{ 
+          throw console.log(`Error - Please submit matching Course Info ID with Assignment Group that references the Course ID.`)
+        }
 
-    for(let i = 0; i < transformed.length; i++){
-      mainResults.push(allTogether(transformed[i], mainMents))
+      }
+      catch(err) {console.log(err)}
     }
+    
+  
 
-
-    return mainResults;
-
-  }
 
 
 console.log(getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions))
