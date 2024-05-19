bc30ab44ef8e4c4f7eaa65153aba049551555d7f
yeahbudd01 <106033471+yeahbudd01@users.noreply.github.com>
2024-05-19 00:10:22 -0700

    Created new try and catch statement and also included the checks for late and early submissions.

--- maincode-sba308.mjs
+++ maincode-sba308.mjs
@@ -45,7 +45,7 @@ const CourseInfo = {
       learner_id: 125,
       assignment_id: 2,
       submission: {
-        submitted_at: "2023-02-12",
+        submitted_at: "2023-02-27",
         score: 150
       }
     },
@@ -61,7 +61,7 @@ const CourseInfo = {
       learner_id: 132,
       assignment_id: 1,
       submission: {
-        submitted_at: "2023-01-24",
+        submitted_at: "2023-01-25",
         score: 39
       }
     },
@@ -69,7 +69,7 @@ const CourseInfo = {
       learner_id: 132,
       assignment_id: 2,
       submission: {
-        submitted_at: "2023-03-07",
+        submitted_at: "2023-02-27",
         score: 140
       }
     }
@@ -103,7 +103,16 @@ function divide(learnerScore, totalScore) { //dividing function. will be used in
 }
 
 function isDue(dueDate, submissionDate) { //if statement used to check due date.
-  return Date.parse(submissionDate) < Date.parse(dueDate);
+  return Date.parse(dueDate) < Date.parse(submissionDate);
+}
+
+function isLate(dueDate, submissionDate) { //if statement used to check if the due date has past and the assignment is late.
+  return Date.parse(dueDate) > Date.parse(submissionDate);
+}
+
+function lateEntry(points){ //function to convert the amount of points that are reduced by 10%
+  let redux = points - (0.1 * points);
+  return redux;
 }
 
 //part 4 put it all together
@@ -112,23 +121,45 @@ function isDue(dueDate, submissionDate) { //if statement used to check due date.
 
 function allTogether(studentObj, assignArr){
   let count = 0;
-  let studentSum = 0;
-  let assignSum = 0;
-  const studentID = studentObj.id;
-  let results = {
+  let studentSum = 0; //student score sum, from the student submissions
+  let assignSum = 0;  //total amount of points sum from the assignment group
+  const studentID = studentObj.id; //grabbing student id from the array of objects.
+  let results = { //declaring final results object. 
     id: studentID
   }
+  try{ //checking to see if the assignments array has an entry of zero using a try catch statement. 
 
-  for(let c in studentObj){
-    if(c == 'id') {continue;}
-    results[c] =  divide(studentObj[c].score, assignArr[count].points_possible);
-    studentSum += studentObj[c].score;
-    assignSum += assignArr[count].points_possible;
-    count++
+    const isZero = assignArr.find(item => item.points_possible === 0) //using find function again to iterate through the assignments array. 
+    if(!isZero){
+  
+      for(let c in studentObj){ //logic for function starts here and will iterate through the transformed student submissions.
+        if(c == 'id') {continue;} //the object will include the student ID and will iterate through it. Setting up if statement if c is equal to the key name 'id.'
+        if(isDue(studentObj[c].submitted_at, assignArr[count].due_at)) { //running the early due date check. 
+          count++ //counting up
+          continue; //skipping entry if it is not due yet. 
+        }
+        else if(isLate(studentObj[c].submitted_at, assignArr[count].due_at)){ //running the late check. 
+          let lateVal = lateEntry(studentObj[c].score) //assigning value to recalculated score that has been late. 
+          results[c] =  divide(lateVal, assignArr[count].points_possible) //doing math before putting it into the results object. 
+          studentSum += lateVal; //upticking the value of student sum using the laveVal var.
+          assignSum += assignArr[count].points_possible;
+          count++
+        }
+        else { //now that the checks have been made, I'm doing the proper math. 
+          results[c] =  divide(studentObj[c].score, assignArr[count].points_possible); 
+          studentSum += studentObj[c].score;
+          assignSum += assignArr[count].points_possible;
+          count++
+        }
 
-  }
-  results.avg = divide(studentSum, assignSum);
-  return results;
+      }
+    results.avg = divide(studentSum, assignSum); //calculating the avg of the total scores.
+    return results; //results obj should now be completed. 
+    } else { 
+      throw console.log(`Error - Please recheck Assignment Group. Score entries cannot be zero.`)//end of catch statement.
+    }
+  } 
+  catch(err) {console.log(err)}
 }
 
 
@@ -136,15 +167,15 @@ function allTogether(studentObj, assignArr){
 function getLearnerData(courseinfo, assignmentgroups, learnersubs) {
 
   //part 1, create an array of objects that takes the assignnments from the assignment groups.  
-  let mainMents = assignmentgroups.assignments;
+  let mainMents = assignmentgroups.assignments; 
   let transformed = transformLearner(learnersubs); //created new variable that takes the new reformatted learnersubs array.
-  let mainResults = [];
+  let mainResults = []; //setting up final array of objects. 
   
   //part 5 implement try catch statement with if else
     try {
       if (courseinfo.id === assignmentgroups.course_id){
-        for(let i = 0; i < transformed.length; i++){
-          mainResults.push(allTogether(transformed[i], mainMents))
+        for(let i = 0; i < transformed.length; i++){ //creating final loop that references the transformed student data, which should also be an array of objects. 
+          mainResults.push(allTogether(transformed[i], mainMents)) //pushing the results of allTogether into the final array. 
         }
         return mainResults;
         } else{ 
